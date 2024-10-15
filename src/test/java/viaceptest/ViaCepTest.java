d1d0518ae2d924ee9f633338ecfbe7e9783ac7e2
unknown <gabriel-h-luz@hotmail.com>
2024-10-15 02:22:20 -0300

    More test cases added

--- src/test/java/viaceptest/ViaCepTest.java
+++ src/test/java/viaceptest/ViaCepTest.java
@@ -32,7 +32,7 @@ public class ViaCepTest {
 
     @Test
     public void testCepInvalido() {
-        String cepInvalido = "00000000";
+        String cepInvalido = "98800";
 
         Response response = given()
                 .baseUri(BASE_URI)
@@ -43,7 +43,55 @@ public class ViaCepTest {
                 .extract()
                 .response();
 
+    }
+
+    @Test
+    public void testCepInexistente() {
+        String cepInexistente = "00000000";
+
+        Response response = given()
+                .baseUri(BASE_URI)
+                .when()
+                .get("/{cep}/json", cepInexistente)
+                .then()
+                .statusCode(400)
+                .extract()
+                .response();
+
         Assertions.assertNotNull(response.jsonPath().get("erro"), "Erro deve estar presente para CEP inválido");
+    }
+
+    @Test
+    public void testCepComLetras() {
+        String cepComLetras = "00abc000";
+
+        Response response = given()
+                .baseUri(BASE_URI)
+                .when()
+                .get("/{cep}/json", cepComLetras)
+                .then()
+                .statusCode(400)
+                .extract()
+                .response();
+    }
+
+    @Test
+    public void testCepComHifen() {
+        String cepComHifen = "01001-000";
+
+        Response response = given()
+                .baseUri(BASE_URI)
+                .when()
+                .get("/{cep}/json", cepComHifen)
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        Assertions.assertNotNull(response.jsonPath().get("logradouro"), "Logradouro não deve ser nulo");
+        Assertions.assertNotNull(response.jsonPath().get("bairro"), "Bairro não deve ser nulo");
+        Assertions.assertNotNull(response.jsonPath().get("localidade"), "Localidade não deve ser nula");
+        Assertions.assertNotNull(response.jsonPath().get("uf"), "UF não deve ser nula");
     }
 }
 
